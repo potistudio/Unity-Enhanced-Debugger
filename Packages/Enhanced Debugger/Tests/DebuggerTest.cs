fc1b99857ebae4946b6f983928196e36ef254f3e
POTI <potistudio2022@gmail.com>
2024-03-03 04:58:53 +0900

    change: Target Instance Class "GameObject" -> "Color"

--- Packages/Enhanced Debugger/Tests/DebuggerTest.cs
+++ Packages/Enhanced Debugger/Tests/DebuggerTest.cs
@@ -4,7 +4,7 @@ using NUnit.Framework;
 public class DebuggerTest {
 	[Test]
 	public void Debug() {
-		System.Reflection.MemberInfo[] members = Debugger.GetMembers (new UnityEngine.GameObject("Test Object"));
+		System.Reflection.MemberInfo[] members = Debugger.GetMembers (new UnityEngine.Color());
 
 		foreach (System.Reflection.MemberInfo member in members) {
 			UnityEngine.Debug.Log (member);
