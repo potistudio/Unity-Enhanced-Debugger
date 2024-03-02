57d687e80588888675f24caf4fc42517bcb111b7
POTI <potistudio2022@gmail.com>
2024-03-03 05:00:15 +0900

    feat: Type Test

--- Packages/Enhanced Debugger/Tests/DebuggerTest.cs
+++ Packages/Enhanced Debugger/Tests/DebuggerTest.cs
@@ -3,6 +3,13 @@ using NUnit.Framework;
 
 public class DebuggerTest {
 	[Test]
+	public void DebugType() {
+		object targetInstance = new System.Collections.Generic.List<float>();
+		UnityEngine.Debug.Log (Debugger.GetType(targetInstance));
+		UnityEngine.Debug.Log (Debugger.GetType(targetInstance).Namespace);
+	}
+
+	[Test]
 	public void DebugMembers() {
 		System.Reflection.MemberInfo[] members = Debugger.GetMembers (new UnityEngine.Color());
 
