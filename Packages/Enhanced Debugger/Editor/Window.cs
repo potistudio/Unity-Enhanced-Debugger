4b483aa382fdf68acf99ab6d78fdcf499f339f33
POTI <potistudio2022@gmail.com>
2024-03-03 07:51:08 +0900

    feat: Custom Window Title and Icon

--- Packages/Enhanced Debugger/Editor/Window.cs
+++ Packages/Enhanced Debugger/Editor/Window.cs
@@ -3,5 +3,9 @@ public class Window : UnityEditor.EditorWindow {
 	[UnityEditor.MenuItem("Window/Enhanced Debugger", priority = 4000)]
 	private static void OpenWindow() {
 		Window window = GetWindow<Window>();
+		window.titleContent = new UnityEngine.GUIContent (
+			"Enhanced Debugger",
+			UnityEditor.EditorGUIUtility.Load ("Profiler.Memory") as UnityEngine.Texture
+		);
 	}
 }
