4bc42658c725be87ae587f5b8673c64aefe0e135
POTI <potistudio2022@gmail.com>
2024-03-03 04:59:28 +0900

    rename: Test Method "Debug" -> "DebugMembers"

--- Packages/Enhanced Debugger/Tests/DebuggerTest.cs
+++ Packages/Enhanced Debugger/Tests/DebuggerTest.cs
@@ -3,7 +3,7 @@ using NUnit.Framework;
 
 public class DebuggerTest {
 	[Test]
-	public void Debug() {
+	public void DebugMembers() {
 		System.Reflection.MemberInfo[] members = Debugger.GetMembers (new UnityEngine.Color());
 
 		foreach (System.Reflection.MemberInfo member in members) {
