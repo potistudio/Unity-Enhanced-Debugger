31879dd6ec793f5cadce180a6a7272c224d95be5
POTI <potistudio2022@gmail.com>
2024-03-03 05:00:50 +0900

    feat: Debug Type

--- Packages/Enhanced Debugger/Runtime/Debugger.cs
+++ Packages/Enhanced Debugger/Runtime/Debugger.cs
@@ -2,9 +2,15 @@
 using System;
 
 public static class Debugger {
-	public static System.Reflection.MemberInfo[] GetMembers (object _obj) {
+	public static Type GetType (object _obj) {
 		Type type = _obj.GetType();
-		System.Reflection.MemberInfo[] members = type.GetMembers ();
+
+		return type;
+	}
+
+	public static System.Reflection.MemberInfo[] GetMembers (object _obj) {
+		Type type = GetType (_obj);
+		System.Reflection.MemberInfo[] members = type.GetMembers();
 
 		return members;
 	}
