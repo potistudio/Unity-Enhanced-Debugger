f222d262bb751851c38be6b0536ce9eb535c18eb
POTI <potistudio2022@gmail.com>
2024-03-03 04:36:33 +0900

    rename: Class "GetMember" -> "GetMembers"

--- Packages/Enhanced Debugger/Runtime/Debugger.cs
+++ Packages/Enhanced Debugger/Runtime/Debugger.cs
@@ -2,7 +2,7 @@
 using System;
 
 public static class Debugger {
-	public static System.Reflection.MemberInfo[] GetMember (object _obj) {
+	public static System.Reflection.MemberInfo[] GetMembers (object _obj) {
 		Type type = _obj.GetType();
 		System.Reflection.MemberInfo[] members = type.GetMembers ();
 
--- Packages/Enhanced Debugger/Tests/DebuggerTest.cs
+++ Packages/Enhanced Debugger/Tests/DebuggerTest.cs
@@ -4,7 +4,7 @@ using NUnit.Framework;
 public class DebuggerTest {
 	[Test]
 	public void Debug() {
-		System.Reflection.MemberInfo[] members = Debugger.GetMember (new UnityEngine.GameObject("Test Object"));
+		System.Reflection.MemberInfo[] members = Debugger.GetMembers (new UnityEngine.GameObject("Test Object"));
 
 		foreach (System.Reflection.MemberInfo member in members) {
 			UnityEngine.Debug.Log (member);
